173ee48c1818fc3e40330c9102a5ca36ff77deb7
Veysel Aksin <veyselaksn@gmail.com>
2023-02-18 17:10:57 +0300

    Message and Sender operations are done

--- pkg/mailer/message.go
+++ pkg/mailer/message.go
@@ -9,81 +9,161 @@ import (
 )
 
 // Message is a struct that contains the information for an email message.
-type Message struct {
-	From        string
-	To          []string
-	CC          []string
-	BCC         []string
-	Subject     string
-	Body        string
-	Attachments map[string][]byte
+type message struct {
+	to          []string
+	cc          []string
+	bcc         []string
+	subject     string
+	body        string
+	attachments map[string][]byte
 }
 
+// IMessage is an interface that defines the methods for an email message.
+// It is used to create a new message, set the To, Cc, Bcc, subject, body, and attachments.
+// The ToBytes method converts the message to a byte array.
+// The SetTo method sets the To field of the message.
+// The GetTo method returns the To field of the message.
+// The SetCc method sets the Cc field of the message.
+// The GetCc method returns the Cc field of the message.
+// The SetBcc method sets the Bcc field of the message.
+// The GetBcc method returns the Bcc field of the message.
+// The SetSubject method sets the subject of the message.
+// The GetSubject method returns the subject of the message.
+// The SetBody method sets the body of the message.
+// The GetBody method returns the body of the message.
+// The SetAttachFiles method attaches a file to the message.
+// The GetAttachFiles method returns the attached files.
 type IMessage interface {
-	// ToBytes() []byte
-	AttachFile(string) error
+	ToBytes() []byte
+	SetTo([]string)
+	GetTo() []string
+	SetCc([]string)
+	GetCc() []string
+	SetBcc([]string)
+	GetBcc() []string
+	SetSubject(string)
+	GetSubject() string
+	SetBody(string)
+	GetBody() string
+	SetAttachFiles(string) error
+	GetAttachFiles() map[string][]byte
 }
 
-var _ IMessage = (*Message)(nil)
+var _ IMessage = (*message)(nil)
 
-// SimpleMessage is a method that creates a new message with the given subject and body.
-// The attachments are optional.
-func NewMessage(subject, body string) *Message {
-	return &Message{Subject: subject, Body: body, Attachments: make(map[string][]byte)}
+// NewMessage is a function that creates a new message. The subject and body are required.
+// The subject and body are strings. The subject is the subject of the email message.
+// The body is the body of the email message. The body can be in HTML format.
+func NewMessage(subject, body string) *message {
+	return &message{subject: subject, body: body, attachments: make(map[string][]byte)}
+}
+
+// SetTo is a method that sets the To field of the message. The To field is a list of email addresses.
+func (m *message) SetTo(to []string) {
+	m.to = to
+}
+
+// GetTo is a method that returns the To field of the message.
+func (m *message) GetTo() []string {
+	return m.to
+}
+
+// SetCc is a method that sets the Cc field of the message. The Cc field is a list of email addresses.
+func (m *message) SetCc(cc []string) {
+	m.cc = cc
+}
+
+// GetCc is a method that returns the Cc field of the message.
+func (m *message) GetCc() []string {
+	return m.cc
+}
+
+// SetBcc is a method that sets the Bcc field of the message. The Bcc field is a list of email addresses.
+func (m *message) SetBcc(bcc []string) {
+	m.bcc = bcc
+}
+
+// GetBcc is a method that returns the Bcc field of the message.
+func (m *message) GetBcc() []string {
+	return m.bcc
+}
+
+// SetSubject is a method that sets the subject of the message. The subject is a string.
+// The SetSubject method is optional because the subject is a required field when creating a new message.
+// SetSubject is used to change the subject of the message.
+func (m *message) SetSubject(subject string) {
+	m.subject = subject
+}
+
+// GetSubject is a method that returns the subject of the message.
+func (m *message) GetSubject() string {
+	return m.subject
+}
+
+// SetBody is a method that sets the body of the message. The body is a string.
+// The SetBody method is optional because the body is a required field when creating a new message.
+// SetBody is used to change the body of the message.
+func (m *message) SetBody(body string) {
+	m.body = body
+}
+
+// GetBody is a method that returns the body of the message.
+func (m *message) GetBody() string {
+	return m.body
 }
 
 // AttachFile is a method that attaches a file to the message. The file is read from the given path.
 // The file name is used as the attachment name.
 // The file content is base64 encoded.
-func (m *Message) AttachFile(src string) error {
+func (m *message) SetAttachFiles(src string) error {
 	content, err := ioutil.ReadFile(src)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	_, fileName := filepath.Split(src)
-	m.Attachments[fileName] = content
+	m.attachments[fileName] = content
 	return nil
+}
+
+func (m *message) GetAttachFiles() map[string][]byte {
+	return m.attachments
 }
 
 // ToBytes is a method that converts the message to a byte array.
 // The byte array is used to send the message to the SMTP server. The byte array is in the format
 // of an email message.
-func (m *Message) ToBytes() []byte {
+func (m *message) ToBytes() []byte {
 	// If there are attachments it will return true otherwise false.
-	hasAttachment := len(m.Attachments) > 0
+	hasAttachment := len(m.attachments) > 0
 
 	// The message is built as a string and then converted to a byte array.
 	var message string = ""
-	message += "From: " + m.From + "\n"
-	message += "To: " + strings.Join(m.To, ";") + "\n"
-	message += "Cc: " + strings.Join(m.CC, ";") + "\n"
-	message += "Bcc: " + strings.Join(m.BCC, ";") + "\n"
-	message += "Subject: " + m.Subject + "\n"
-	message += "Body: " + m.Body + "\n"
+	message += "To: " + strings.Join(m.GetTo(), ";") + "\n"
+	message += "Cc: " + strings.Join(m.GetCc(), ";") + "\n"
+	message += "Bcc: " + strings.Join(m.GetBcc(), ";") + "\n"
+	message += "Subject: " + m.GetSubject() + "\n"
+	message += "Body: " + m.GetBody() + "\n"
 	message += "MIME-Version: 1.0\n"
 
 	// If there are attachments, the message is in the multipart/mixed format.
 	// Otherwise, the message is in the text/plain format.
 	if hasAttachment {
-		message += "Content-Type: multipart/mixed; boundary=frontier\n"
-		message += "This is a multi-part message in MIME format.\n"
-		message += "--frontier\n"
-		message += "Content-Type: text/plain; charset=us-ascii\n"
-		message += "Content-Transfer-Encoding: 7bit\n"
-		message += m.Body + "\n"
-		message += "--frontier\n"
-		for k, v := range m.Attachments {
+		message += "Content-Type: multipart/mixed; boundary=\"BOUNDARY\"\n\n"
+		message += "--BOUNDARY\n"
+		message += "Content-Transfer-Encoding: base64\n"
+		for k, v := range m.GetAttachFiles() {
+			message += "--BOUNDARY\n"
 			message += "Content-Type: " + http.DetectContentType(v) + "; name=\"" + k + "\"\n"
 			message += "Content-Transfer-Encoding: base64\n"
 			message += "Content-Disposition: attachment; filename=\"" + k + "\"\n"
 			message += base64.StdEncoding.EncodeToString(v) + "\n"
-			message += "--frontier\n"
 		}
+
+		message += "--BOUNDARY--\n"
 	} else {
 		message += "Content-Type: text/plain; charset=utf-8\n"
-		message += "Content-Transfer-Encoding: 7bit\n"
-		message += m.Body + "\n"
+		message += m.GetBody() + "\n"
 	}
 
 	return []byte(message)
--- pkg/mailer/sender.go
+++ pkg/mailer/sender.go
@@ -1,1 +1,115 @@
 package mailer
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net/smtp"
+)
+
+// Authentication is a struct that contains the authentication information for the SMTP server.
+// It is used to create a new Sender. The fields are:
+//
+//	Username: The username for the SMTP server.
+//	Password: The password for the SMTP server.
+//	Host: The host for the SMTP server.
+//	Port: The port for the SMTP server.
+type Authentication struct {
+	Username string
+	Password string
+	Host     string
+	Port     string
+}
+
+// SMTPDialer is a struct that contains the dialer information for the SMTP server.
+// SMTPDialer is used for servers that do not require authentication.
+// It is used to create a new Sender. The fields are:
+//
+//	Host: The host for the SMTP server.
+//	Port: The port for the SMTP server.
+//
+// If the SMTP server does not require authentication, then the Authentication struct is not needed.
+type SMTPDialer struct {
+	Host string
+	Port string
+}
+
+// ISender is an interface that defines the methods for a sender.
+// It is used to create a new sender, and send an email message.
+// The SendMail method sends an email message.
+type ISender interface {
+	SendMail(m *message) error
+	SendMailTLS(m *message, tlsconfig *tls.Config) error
+}
+
+// AuthType is a type that defines the authentication type for the sender.
+type AuthType string
+
+// Sender is a struct that contains the authentication information for the SMTP server.
+type sender struct {
+	plainAuth *smtp.Auth
+	client    *smtp.Client
+	authType  AuthType
+	auth      *Authentication
+}
+
+var _ ISender = (*sender)(nil)
+
+// NewPlainAuth is a function that creates a new sender. The Authentication struct is required.
+// The Authentication struct contains the authentication information for the SMTP server.
+// The Authentication struct is used to create a new Sender. The fields are:
+//
+//	Username: The username for the SMTP server.
+//	Password: The password for the SMTP server.
+//	Host: The host for the SMTP server.
+//	Port: The port for the SMTP server.
+func NewPlainAuth(auth *Authentication) *sender {
+	plainAuth := smtp.PlainAuth("", auth.Username, auth.Password, auth.Host)
+	return &sender{plainAuth: &plainAuth, authType: "plain", auth: auth}
+}
+
+// NewDialer is a function that creates a new sender. The SMTPDialer struct is required.
+// The SMTPDialer struct contains the dialer information for the SMTP server.
+// SMTPDialer is used for servers that do not require authentication.
+// It is used to create a new Sender. The fields are:
+//
+//	Host: The host for the SMTP server.
+//	Port: The port for the SMTP server.
+func NewDialer(smtpDialer *SMTPDialer) *sender {
+	client, _ := smtp.Dial(smtpDialer.Host + ":" + smtpDialer.Port)
+	return &sender{client: client, authType: "dialer"}
+}
+
+// SendMail is a method that sends an email message.
+// The message struct is required.
+// The message struct contains the email message information.
+// The message struct is used to create a new email message.
+func (s *sender) SendMail(m *message) error {
+	return smtp.SendMail(fmt.Sprintf("%s:%s", s.auth.Host, s.auth.Port), *s.plainAuth, s.auth.Username, m.GetTo(), m.ToBytes())
+}
+
+// SendMailTLS is a method that sends an email message. The message struct is required and the tls.Config struct is required.
+// The message struct contains the email message information.
+// The tls.Config struct contains the TLS configuration information.
+func (s *sender) SendMailTLS(m *message, tlsConfig *tls.Config) error {
+	s.client.StartTLS(tlsConfig)
+
+	// Set the sender and recipient first before calling Data() to send the email.
+	if err := s.client.Mail(s.auth.Username); err != nil {
+		return err
+	}
+
+	for _, k := range m.GetTo() {
+		s.client.Rcpt(k)
+	}
+
+	// Send the email body. The message is sent in the same format as the RFC 822.
+	w, err := s.client.Data()
+	if err != nil {
+		return err
+	}
+
+	w.Write(m.ToBytes())
+	w.Close()
+
+	return nil
+}
